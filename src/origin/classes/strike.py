3eb873af1f8c3cf21d610a83cbad605d85ed0ce9
Soltan Adil <adil01king@mail.ru>
2021-12-30 02:34:18 +0600

    Появление правильного выстрела и начало класса железных блоков

--- src/origin/classes/strike.py
+++ src/origin/classes/strike.py
@@ -13,29 +13,31 @@ from ..helpers.variables import *
 
 class Strike(pygame.sprite.Sprite):
     """Класс выстрела"""
-    def __init__(self, tank_pos, mouse_pos):
+    def __init__(self, pos, mouse_pos):
         super().__init__(ALL_SPRITES)
         radius = 10
+        self.destination = mouse_pos
+        self.pos = pos
         self.add(STRIKE_GROUP)
         self.image = pygame.Surface((2 * radius, 2 * radius),
                                     pygame.SRCALPHA, 32)
-        self.rect = pygame.Rect(*tank_pos, 2 * radius, 2 * radius)
+        self.rect = pygame.Rect(*pos, 2 * radius, 2 * radius)
         self.current_angle = 0
         pygame.draw.circle(self.image, pygame.Color("red"),
                            (radius, radius), radius)
-        self.vx, self.vy = (mouse_pos[0] - tank_pos[0]) // 3, (mouse_pos[1] - tank_pos[1]) // 3
-        self.pos = (3, 3)
-        self.step = 10
-        print(self.vx, self.vy)
+        self.vx, self.vy = 0, 0
+        self.vector = (self.destination[0] - self.pos[0],
+                       self.destination[1] - self.pos[1])
+        self.hypotenuse = (self.vector[0] ** 2 + self.vector[1] ** 2) ** 0.5
+        print(self.vector)
 
     def update(self, *args):
-        step_x, step = 0, 0
-        if self.vx > 0:
-            self.vx -= 1
-            step_x = self.step
-        if self.vy > 0:
-            self.vy -= 1
-            step = self.step
-        if pygame.sprite.spritecollideany(self, TEXTURE_GROUP):
-            self.kill()
-        self.rect = self.rect.move(step_x, step)
+        self.vx = round((self.vector[0] * 10) / self.hypotenuse)
+        self.vy = round((self.vector[1] * 10) / self.hypotenuse)
+        # if self.destination[0] < self.pos[0]:
+        #     self.vx = -1 * self.vx
+        # if self.destination[1] < self.pos[1]:
+        #     self.vy = -1 * self.vy
+        # if pygame.sprite.spritecollideany(self, TEXTURE_GROUP):
+        #     self.kill()
+        self.rect = self.rect.move(self.vx, self.vy)
